33d00eb73041c590a6166938f3b2fd09d994b6b6
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-07 17:43:28 +0530

    [Mihir] Add functionality to validate first name of user

---
+++ RegexUserRegistration/Patterns.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace RegexUserRegistration
+{
+    class Patterns
+    {
+        string firstName = "^[A-Z][a-z0-9A-Z]{3,}";
+        public void validateFirstName(string fname)
+        {
+            if (Regex.IsMatch(fname, firstName))
+            {
+                Console.WriteLine("First Name is valid");
+            }
+            else
+            {
+                Console.WriteLine("First Name not valid");
+            }
+        }
+    }
+}
--- RegexUserRegistration/Program.cs
+++ RegexUserRegistration/Program.cs
@@ -7,6 +7,10 @@ namespace RegexUserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Regex User Registration Problem");
+            Patterns pattern = new Patterns();
+            Console.WriteLine("Enter the first name");
+            string fname = Console.ReadLine();
+            pattern.validateFirstName(fname);
         }
     }
 }
