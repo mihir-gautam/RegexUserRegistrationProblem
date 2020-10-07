0d0f878b70fc397f88c24ffc68e9c5466fa59c14
Mihir Gautam <Mihir Gautam@DESKTOP-JPP42H1>
2020-10-07 17:29:41 +0530

    [Mihir] Add Welcome to the User Registration Problem

---
+++ RegexUserRegistration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RegexUserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Regex User Registration Problem");
+        }
+    }
+}
